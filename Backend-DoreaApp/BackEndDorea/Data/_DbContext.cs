5c197c0203ebda8dab1c19e64217118d09630dc5
jonathan-nascimento51 <jonathanmoletta2010@hotmail.com>
2022-11-13 19:54:25 -0300

    Subindo backend

---
+++ Backend-DoreaApp/BackEndDorea/Data/_DbContext.cs
@@ -0,0 +1,17 @@
+﻿using BackEndDorea.Model;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Reflection.Emit;
+
+namespace BackEndDorea.Data
+{
+    public class _DbContext : DbContext
+    {
+        public _DbContext(DbContextOptions<_DbContext> options) : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        public DbSet<Doacao> Doacaos { get; set; }
+    }
+}
---
+++ Backend-DoreaApp/BackEndDorea/Model/Doacao.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BackEndDorea.Model
+{
+    [Table(name: "doacao")]
+    public class Doacao
+    {
+        [Column("doacao_id")]
+        public int Id { get; set; }
+
+        [Column("doacao_valor")]
+        public string? Valor { get; set; }
+    }
+}
