a1c50c97a379d77ff193b7ae2e0fd0a9770ed00c
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-15 12:43:08 -0300

    add código html e css do component parceiros

---
+++ Frontend-DoreaApp/src/components/parceiros/Parceiros.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+function Parceiros() {
+    return (
+        <div>
+            <main className='container d-flex align-items-center' id='main-parceiros'>
+                <div className='container pt-5'>
+                    <section className='mb-12 mt-6 text-center'>
+                        <h2 className='fw-bold mb-5'>Nossos&nbsp;<span id='txt-color2'>Parceiros</span></h2>
+                    </section>
+                    <div className='row mb-lg-5'>
+                        <div className='col-sm-6 p-4 justify-content-center' >
+                            <p className='pt-lg-5'>
+                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusantium quis asperiores aliquam
+                                debitis eaque consectetur. Reiciendis sint ullam, perferendis laboriosam facere quae quos ipsum
+                                ea nihil, id aperiam voluptatibus expedita!
+                            </p>
+                        </div>
+                        <div className='col-sm-6 mb-3 mt-3'>
+                            <div className='row'>
+                                <div className='col-6 mb-5 mb-lg-0'>
+                                    <img src='https://live.staticflickr.com/65535/52501391075_8d44e2c99f_o.png' className='img-fluid grayscale px-4 px-md-5' alt='' aria-controls='#picker-editor' />
+                                </div>
+                                <div className='col-6 col-6 mb-5 mb-lg-0'>
+                                    <img src='https://live.staticflickr.com/65535/52501468223_99eb03a670_o.png' className='img-fluid grayscale px-4 px-md-5' alt='' aria-controls='#picker-editor' />
+                                </div>
+                                <div className='row-2 col-6 mt-5 mb-lg-0'>
+                                    <img src='https://live.staticflickr.com/65535/52501194559_0350443fdb_o.png' className='img-fluid grayscale px-4 px-md-5' alt='' aria-controls='#picker-editor' />
+                                </div>
+                                <div className='row-2 col-6 mt-5 mb-lg-0'>
+                                    <img src='https://live.staticflickr.com/65535/52501391105_9fee09717c_o.png' className='img-fluid grayscale px-4 px-md-5' alt='' aria-controls='#picker-editor' />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
+        </div>
+    )
+}
+
+export default Parceiros
