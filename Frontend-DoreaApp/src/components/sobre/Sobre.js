1678dd1af7d0fb518f9e36cab4120a68de715235
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-15 12:42:04 -0300

    alteração nas linhas do código

--- Frontend-DoreaApp/src/components/sobre/Sobre.js
+++ Frontend-DoreaApp/src/components/sobre/Sobre.js
@@ -1,18 +1,18 @@
 import React from 'react'
 
-export default function Sobre() {
+function Sobre() {
     return (
         <div>
             <main id='sobre'>
                 <div className='main-sobre'>
-                    <div className='h-50 d-flex align-items-center bg-img pt-5'>
+                    <div className='h-50 d-flex align-items-center pt-5'>
                         <div className='container'>
                             <div className='row'>
                                 <div className='mx-auto text-center col-md-6'>
                                     <h1 className='display-1 text-white'>Sobre a Doreá</h1>
                                     <p className='lead text-primary'>
                                         &nbsp;Somos um projeto que nasceu na
-                                        <a href='https://recode.org.br/'>Recode</a> em 2022, com intuito
+                                        <a href='https://recode.org.br/'> Recode </a> em 2022, com intuito
                                         de contribuir para a agenda de desenvolvimento sustentável da
                                         onu.
                                     </p>
@@ -21,24 +21,24 @@ export default function Sobre() {
                         </div>
                     </div>
                 </div>
-                <div className='py-5'>
+                <div className='py-5' id='sobre-valores'>
                     <div className='container'>
                         <div className='row'>
                             <div className='p-4 col-lg-4 col-md-6'>
                                 <h4><b>Missão</b></h4>
-                                <p className='fw-semibold'>
+                                <p>
                                     Reduzir o acesso desigual aos recursos escolares.
                                 </p>
                             </div>
                             <div className='p-4 col-lg-4 col-md-6'>
                                 <h4><b>Visão</b></h4>
-                                <p className='fw-semibold'>
+                                <p>
                                     Suprir a desigualdade e promover o acesso aos estudos.
                                 </p>
                             </div>
                             <div className='p-4 col-lg-4'>
                                 <h4><b>Valores</b></h4>
-                                <p className='fw-semibold'>
+                                <p>
                                     Gerar oportunidades á todos, com igualdade e equidade.
                                 </p>
                             </div>
@@ -49,3 +49,5 @@ export default function Sobre() {
         </div>
     )
 }
+
+export default Sobre
