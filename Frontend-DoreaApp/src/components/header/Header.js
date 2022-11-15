730b23254ac6a67c1ef2f1b58fd7b17660e2633d
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-15 12:45:33 -0300

    Alterado linhas do código header

--- Frontend-DoreaApp/src/components/header/Header.js
+++ Frontend-DoreaApp/src/components/header/Header.js
@@ -6,14 +6,14 @@ const Header = () => {
             <header id='header'>
                 <nav className='navbar navbar-expand-lg'>
                     <div className='container-fluid'>
-                        <a className='navbar-brand' href='#'>DOREÁ</a>
+                        <a className='navbar-brand' href='/'>DOREÁ</a>
                         <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarContent' aria-controls='navbarContent' aria-expanded='false' aria-label='Toggle navigation' >
                             <span className='navbar-toggler-icon'></span>
                         </button>
                         <div className='collapse navbar-collapse' id='navbarContent'>
                             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
                                 <li className='nav-item'>
-                                    <a className='nav-link active' aria-current='page' href='/?'>Home</a>
+                                    <a className='nav-link active' aria-current='page' href='/'>Home</a>
                                 </li>
                                 <li className='nav-item'>
                                     <a className='nav-link' href='/Sobre'>Sobre</a>
@@ -26,11 +26,11 @@ const Header = () => {
                                         <li><a className='dropdown-item' href='#'>Recentes</a></li>
                                         <li><a className='dropdown-item' href='#'>Como Fazer</a></li>
                                         <li><hr className='dropdown-divider' /></li>
-                                        <li><a className='dropdown-item' href='#'>Parceiros</a></li>
+                                        <li><a className='dropdown-item' href='/Parceiros'>Parceiros</a></li>
                                     </ul>
                                 </li>
                                 <li className='nav-item'>
-                                    <a className='nav-link' href=''>Contato</a>
+                                    <a className='nav-link' href='/Contato'>Contato</a>
                                 </li>
                             </ul>
                             <form className='d-flex'>
