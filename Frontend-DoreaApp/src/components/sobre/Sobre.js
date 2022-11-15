64e51af3e1e9fdd5e651e15cca371c08208b6562
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-14 23:27:08 -0300

    alterações no component sobre

--- Frontend-DoreaApp/src/components/sobre/Sobre.js
+++ Frontend-DoreaApp/src/components/sobre/Sobre.js
@@ -10,7 +10,7 @@ export default function Sobre() {
             a todos.</p>
           <figure class="col-md-3 col-sm-6">
             <div class="">
-              <img src="../img/ilustração-crinança-escola.png" alt="desenho duas crianças indo a escola" class=" img-fluid">
+              <img src="../img/ilustração-crinança-escola.png" alt="desenho duas crianças indo a escola" class=" img-fluid"/>
             </div>
           </figure>
         </article>
@@ -18,7 +18,7 @@ export default function Sobre() {
         <article class="tex-center pt-4 row d-flex justify-content-center align-items-center">
           <h2 class="text-uppercase fw-bold 12 align-items-end p-2">Missão</h2>
           <figure class="col-md-3 col-sm-6">
-            <img src="../img/mapa-do-tesouro.png" alt="mapa do tesouro" class="img-fluid">
+            <img src="../img/mapa-do-tesouro.png" alt="mapa do tesouro" class="img-fluid"/>
           </figure>
           <p class="fw-semibold image.png col-md-3 col-sm-6 fs-5">Reduzir o afastamento escolar causado pela falta de locomoção para estudantes e garantir a segurança do jovem no caminho para a escola.  </p>
         </article>
@@ -27,7 +27,7 @@ export default function Sobre() {
           <h2 class="text-uppercase fw-bold align-items-end p-2">Visão</h2>
           <p class="fw-semibold col-md-3 col-sm-6 fs-5">Atender em todo o Brasil e alcançar o maior numero de jovens possiveis</p>
           <figure class="col-md-3 col-sm-6">
-            <img src="../img/desenho-visão.png" alt="oculos-laranja" class="img-fluid">
+            <img src="../img/desenho-visão.png" alt="oculos-laranja" class="img-fluid"/>
           </figure>
         </article>
 
