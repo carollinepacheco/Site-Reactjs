a61285c414771b1f21967f30a975158168105e24
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-15 01:09:39 -0300

    modificado linhas de codigo

--- Frontend-DoreaApp/src/components/sobre/Sobre.js
+++ Frontend-DoreaApp/src/components/sobre/Sobre.js
@@ -1,49 +1,51 @@
 import React from 'react'
 
 export default function Sobre() {
-  return (
-    <div>
-      <main id='sobre' className='main-tamanho'>
-        <div className='h-50 d-flex align-items-center bg-img pt-5'>
-          <div className='container'>
-            <div className='row'>
-              <div className='mx-auto text-center col-md-6'>
-                <h1 className='display-1 text-white'>Sobre a Doreá</h1>
-                <p className='lead text-primary'>
-                  &nbsp;Somos um projeto que nasceu na
-                  <a href='https://recode.org.br/'>Recode</a> em 2022, com intuito
-                  de contribuir para a agenda de desenvolvimento sustentável da
-                  onu.
-                </p>
-              </div>
-            </div>
-          </div>
+    return (
+        <div>
+            <main>
+                <div id='sobre' className='main-sobre'>
+                    <div className='h-50 d-flex align-items-center bg-img pt-5'>
+                        <div className='container'>
+                            <div className='row'>
+                                <div className='mx-auto text-center col-md-6'>
+                                    <h1 className='display-1 text-white'>Sobre a Doreá</h1>
+                                    <p className='lead text-primary'>
+                                        &nbsp;Somos um projeto que nasceu na
+                                        <a href='https://recode.org.br/'>Recode</a> em 2022, com intuito
+                                        de contribuir para a agenda de desenvolvimento sustentável da
+                                        onu.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className='py-5'>
+                    <div className='container'>
+                        <div className='row'>
+                            <div className='p-4 col-lg-4 col-md-6'>
+                                <h4><b>Missão</b></h4>
+                                <p className='fw-semibold'>
+                                    Reduzir o acesso desigual aos recursos escolares.
+                                </p>
+                            </div>
+                            <div className='p-4 col-lg-4 col-md-6'>
+                                <h4><b>Visão</b></h4>
+                                <p className='fw-semibold'>
+                                    Suprir a desigualdade e promover o acesso aos estudos.
+                                </p>
+                            </div>
+                            <div className='p-4 col-lg-4'>
+                                <h4><b>Valores</b></h4>
+                                <p className='fw-semibold'>
+                                    Gerar oportunidades á todos, com igualdade e equidade.
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </main>
         </div>
-        <div className='py-5'>
-          <div className='container'>
-            <div className='row'>
-              <div className='p-4 col-lg-4 col-md-6'>
-                <h4><b>Missão</b></h4>
-                <p className='fw-semibold'>
-                  Reduzir o acesso desigual aos recursos escolares.
-                </p>
-              </div>
-              <div className='p-4 col-lg-4 col-md-6'>
-                <h4><b>Visão</b></h4>
-                <p className='fw-semibold'>
-                  Suprir a desigualdade e promover o acesso aos estudos.
-                </p>
-              </div>
-              <div className='p-4 col-lg-4'>
-                <h4><b>Valores</b></h4>
-                <p className='fw-semibold'>
-                  Gerar oportunidades á todos, com igualdade e equidade.
-                </p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </main>
-    </div>
-  )
+    )
 }
