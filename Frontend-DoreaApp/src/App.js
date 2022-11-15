1eb47d9bfba490a62dc608736e00f33d32dbcdb1
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-15 12:46:34 -0300

    add novas rotas

--- Frontend-DoreaApp/src/App.js
+++ Frontend-DoreaApp/src/App.js
@@ -5,6 +5,8 @@ import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
 import Main from './components/main/Main';
 import Sobre from './components/sobre/Sobre';
+import Parceiros from './components/parceiros/Parceiros';
+import Contato from './components/contato/Contato';
 
 function App() {
   return (
@@ -12,8 +14,10 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
-          <Route exact path="/" element={<Main />}></Route>
-          <Route path = "/Sobre" element = {<Sobre />}></Route>
+          <Route exact path='/' element={<Main />}></Route>
+          <Route path='/Sobre' element={<Sobre />}></Route>
+          <Route path='/Parceiros' element={<Parceiros />}></Route>
+          <Route path='/Contato' element={<Contato />}></Route>
         </Routes>
         <Footer />
       </BrowserRouter>
