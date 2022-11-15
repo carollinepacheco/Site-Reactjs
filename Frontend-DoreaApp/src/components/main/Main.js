c68cf71c7a28d75c5d1629be41e852c22d6a43da
Caroline Pacheco <carolline.pacheco@gmail.com>
2022-11-15 12:44:12 -0300

    alteração nas linhas do código, add svg ondas

--- Frontend-DoreaApp/src/components/main/Main.js
+++ Frontend-DoreaApp/src/components/main/Main.js
@@ -7,86 +7,112 @@ function Main() {
                 <div className='jumbotron card card-image jumbotron-img'>
                     <div className='text-white text-center py-5 px-4'>
                         <div>
-                            <h2 className='card-title h1-responsive pt-3 mb-5 font-bold'><strong>Create your beautiful website with Bootstrap</strong></h2>
-                            <p className='mx-5 mb-5'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repellat fugiat, laboriosam, voluptatem,
-                                optio vero odio nam sit officia accusamus minus error nisi architecto nulla ipsum dignissimos. Odit sed qui, dolorum!
+                            <h2 className='card-title h1-responsive pt-3 mb-5 font-bold'><strong>Doreá</strong></h2>
+                            <p className='mx-5 mb-5'>Projeto voltado para arrecadação e doação de fundos para ONGS de Educação!
                             </p>
-                            <a className='btn btn-outline-light btn-md' href='#' role='button'><i className='bi bi-collection-fill'></i>Leia mais</a>
+                            <a className='btn btn-dark btn-rounded btn-md' id='btnLeiaMais' href='/Sobre' role='button'><i className='bi bi-collection-fill'></i>Leia mais</a>
                         </div>
                     </div>
                 </div>
+
                 <div className='jumbotron card' id='jumbotron-card'>
-                    <div className='text-white text-center py-5 px-4'>
+                    <svg id='svgOndas'
+                        data-name="Layer 1"
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 1200 120"
+                        preserveAspectRatio="none"
+                        style={{
+                            opacity: '0.83',
+                            width: '100%',
+                            height: '130px',
+                            fill: '#62c6df',
+                            transform: 'rotateY(180deg)'
+                        }}>
+                        <path d='M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z'></path>
+                    </svg>
+                    <div className='text-white text-center py-5 px-4 pt-0'>
                         <h3>NOSSOS PROJETOS</h3>
-                        <p>Venha conhecer</p>
+                        <h2>Venha conhecer</h2>
                     </div>
                 </div>
-                <div className='container-fluid mb-3' id='cards-doacao'>
-                    <div className="row row-cols-1 row-cols-md-4 g-4">
-                        <div className="col">
-                            <div className='card h-100' style={{ width: '18rem' }}>
-                                <img src="https://live.staticflickr.com/65535/52500414261_6a273493c8_b.jpg" className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                </div>
-                                <div className='card-footer d-grid'>
-                                    <a href="#" className="btn btn-primary">Doar</a>
-                                </div>
-                                <div className="progress">
-                                    <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ width: '25%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100">25%</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col">
-                            <div className='card h-100' style={{ width: '18rem' }}>
-                                <img src="https://live.staticflickr.com/65535/52500414261_6a273493c8_b.jpg" className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                </div>
-                                <div className='card-footer'>
-                                    <a href="#" className="btn btn-primary d-grid">Doar</a>
-                                </div>
-                                <div className="progress">
-                                    <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ width: '75%' }} aria-valuenow="75" aria-valuemin="0" aria-valuemax="100">75%</div>
+                <div class='container-fluid mb-3' id='cards-doacao'>
+                    <div class='row gx-3 justify-content-center'>
+                        <div class='col-lg-3 col-md-6 pt-3'>
+                            <div class='card'>
+                                <img src='https://live.staticflickr.com/6147/5951651981_4b0aea3b2d_o.jpg' height='200px' class='card-img-top' alt='computadores' />
+                                <div class='card-body'>
+                                    <h5 class='card-title'>Doação de Computadores</h5>
+                                    <p class='card-text'>
+                                        Some quick example text to build on the card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <div className='card-footer d-grid'>
+                                        <a href='#' className='btn btn-primary'>Doar</a>
+                                    </div>
+                                    <div className='progress'>
+                                        <div className='progress-bar progress-bar-striped bg-success' role='progressbar' style={{ width: '25%' }} aria-valuenow='25' aria-valuemin='0' aria-valuemax='100'>25%</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className='card h-100' style={{ width: '18rem' }}>
-                                <img src="https://live.staticflickr.com/65535/52500414261_6a273493c8_b.jpg" className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                                </div>
-                                <div className='card-footer'>
-                                    <a href="#" className="btn btn-primary d-grid">Doar</a>
-                                </div>
-                                <div className="progress">
-                                    <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ width: '45%' }} aria-valuenow="45" aria-valuemin="0" aria-valuemax="100">45%</div>
+                        <div class='col-lg-3 col-md-6 pt-3'>
+                            <div class='card'>
+                                <img src='https://live.staticflickr.com/7376/16395045085_d3391ab068_o.jpg' height='200px' class='card-img-top' alt='livros' />
+                                <div class='card-body'>
+                                    <h5 class='card-title'>Doação de Livros</h5>
+                                    <p class='card-text'>
+                                        Some quick example text to build on the card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <div className='card-footer d-grid'>
+                                        <a href='#' className='btn btn-primary'>Doar</a>
+                                    </div>
+                                    <div className='progress'>
+                                        <div className='progress-bar progress-bar-striped bg-success' role='progressbar' style={{ width: '45%' }} aria-valuenow='45' aria-valuemin='0' aria-valuemax='100'>45%</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div className="col">
-                            <div className='card h-100' style={{ width: '18rem' }}>
-                                <img src="https://live.staticflickr.com/65535/52500414261_6a273493c8_b.jpg" className="card-img-top" alt="..." />
-                                <div className="card-body">
-                                    <h5 className="card-title">Card title</h5>
-                                    <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+                        <div class='col-lg-3 col-md-6 pt-3'>
+                            <div class='card'>
+                                <img src='https://live.staticflickr.com/7005/6540872225_553bbd183e_o.jpg' height='200px' class='card-img-top' alt='cesta basica' />
+                                <div class='card-body'>
+                                    <h5 class='card-title'>Doação de Alimentos</h5>
+                                    <p class='card-text'>
+                                        Some quick example text to build on the card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <div className='card-footer d-grid'>
+                                        <a href='#' className='btn btn-primary'>Doar</a>
+                                    </div>
+                                    <div className='progress'>
+                                        <div className='progress-bar progress-bar-striped bg-success' role='progressbar' style={{ width: '50%' }} aria-valuenow='50' aria-valuemin='0' aria-valuemax='100'>50%</div>
+                                    </div>
                                 </div>
-                                <div className='card-footer'>
-                                    <a href="#" className="btn btn-primary d-grid">Doar</a>
-                                </div>
-                                <div className="progress">
-                                    <div className="progress-bar progress-bar-striped bg-success" role="progressbar" style={{ width: '50%' }} aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">50%</div>
+                            </div>
+                        </div>
+                        <div class='col-lg-3 col-md-6 pt-3'>
+                            <div class='card'>
+                                <img src='https://live.staticflickr.com/65535/51631991833_ab0243f021_o.jpg' height='200px' class='card-img-top' alt='Fissure in Sandstone' />
+                                <div class='card-body'>
+                                    <h5 class='card-title'>Doação de Absorventes</h5>
+                                    <p class='card-text'>
+                                        Some quick example text to build on the card title and make up the bulk of the
+                                        card's content.
+                                    </p>
+                                    <div className='card-footer d-grid'>
+                                        <a href='#' className='btn btn-primary'>Doar</a>
+                                    </div>
+                                    <div className='progress'>
+                                        <div className='progress-bar progress-bar-striped bg-success' role='progressbar' style={{ width: '70%' }} aria-valuenow='70' aria-valuemin='0' aria-valuemax='100'>70%</div>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </main>
-        </div>
+            </main >
+        </div >
     )
 }
 
